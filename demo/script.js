f8ed69fd5feffe733c1a5697e9dca61d129ddedf
AakashEr <rk266985@gmail.com>
2022-10-05 00:54:46 +0530

    Uploading

---
+++ demo/script.js
@@ -0,0 +1,8 @@
+let namasteBtn = document.querySelector('button');
+namasteBtn.addEventListener('click', showwMsg);
+
+function showMsg() {
+    // let name = prompt('Enter Name of Student');
+    // namasteBtn.textContent = 'Roll No. 1:' + name;
+    Alert("Hello to Aakash world");
+}
